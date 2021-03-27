b84c9ecbc8e2e7e9afb06bb317759a3cc0e1a4ba
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-03-27 14:50:53 +0800

    add page Cart.js, Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Home from './pages/Home'
 import Product from './pages/Product'
+import Cart from './pages/Cart'
 
 import { StoreProvider } from "./store";
 
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/:pageName" component={Home} />
           <Route path="/product/:productId" component={Product} />
+          <Route path="/cart" component={Cart}/>
         </Switch>
       </BrowserRouter>
     </StoreProvider>
